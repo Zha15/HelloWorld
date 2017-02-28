71d71816652c4371d3bc7226161a89a08345061e
Mustika Dwi Hidayati <zhaatv@gmail.com>
2017-02-28 18:41:15 +0700

    Add files via upload

---
+++ Tugas 2 - Mahasiswa/Mahasiswa.java
@@ -0,0 +1,7 @@
+public class Mahasiswa {
+	public static void main (String [] argv){
+		System.out.println("Nama Lengkap	: Mustika Dwi Hidayati");
+		System.out.println("NIM		: 10115190");
+		System.out.println("Alamat		: Jl. Tubagus Ismail Bawah, Bandung Kota");
+	}
+}
