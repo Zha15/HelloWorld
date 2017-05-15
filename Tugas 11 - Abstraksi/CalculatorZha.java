6f917487aee45a168ec804978ed221ef668e3794
Mustika Dwi Hidayati <zhaatv@gmail.com>
2017-05-15 18:23:03 +0700

    Add files via upload

---
+++ Tugas 11 - Abstraksi/CalculatorZha.java
@@ -0,0 +1,20 @@
+public class CalculatorZha extends Calculator {
+
+	
+
+	protected double hasilTambah(double x, double y){
+		return x+y;
+	}
+	
+	protected double hasilKurang(double x, double y){
+		return x-y;
+	}
+	
+	protected double hasilKali(double x, double y){
+		return x*y;
+	}
+	
+	protected double hasilBagi(double x, double y){
+		return x/y;
+	}
+}
